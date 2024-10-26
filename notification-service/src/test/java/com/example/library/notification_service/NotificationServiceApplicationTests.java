d47359a209b35e0716631332bff256a9ed2902e4
93350anwesha <2023mt93350@wilp.bits-pilani.ac.in>
2024-10-26 22:10:27 +0530

    Initial commit with parent POM and microservices

---
+++ notification-service/src/test/java/com/example/library/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
